12886077c63a18a967da012e02ad412481142598
Gabriel Penteado <105011647+penteado-git@users.noreply.github.com>
2022-05-23 18:31:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,39 +9,36 @@ const html = {
   btns: [...doc(".btns").children],
 };
 
-function isCPF(numCpf = 0) {
-  numCpf = numCpf.replace(/\.|-/g, "");
-  let soma = 0;
-  soma += cpf[0] * 10;
-  soma += cpf[1] * 9;
-  soma += cpf[2] * 8;
-  soma += cpf[3] * 7;
-  soma += cpf[4] * 6;
-  soma += cpf[5] * 5;
-  soma += cpf[6] * 4;
-  soma += cpf[7] * 3;
-  soma += cpf[8] * 2;
-  soma = (soma * 10) % 11;
-  if (soma == 10 || soma == 11) soma = 0;
-  
-  if (soma != cpf[9]) return false;
+function isCPF() {
+  var cpf = doc("#cpf").value;
+  cpf = cpf.replace(/[^\d]+/g, "");
+  if (cpf == "") return false;
+  if (
+    cpf.length != 11 ||
+    cpf == "00000000000" ||
+    cpf == "11111111111" ||
+    cpf == "22222222222" ||
+    cpf == "33333333333" ||
+    cpf == "44444444444" ||
+    cpf == "55555555555" ||
+    cpf == "66666666666" ||
+    cpf == "77777777777" ||
+    cpf == "88888888888" ||
+    cpf == "99999999999"
+  )
+    return false;
 
-  soma = 0;
-  soma += cpf[0] * 11;
-  soma += cpf[1] * 10;
-  soma += cpf[2] * 9;
-  soma += cpf[3] * 8;
-  soma += cpf[4] * 7;
-  soma += cpf[5] * 6;
-  soma += cpf[6] * 5;
-  soma += cpf[7] * 4;
-  soma += cpf[8] * 3;
-  soma += cpf[9] * 2;
-  soma = (soma * 10) % 11;
-  if (soma == 10 || soma == 11) soma = 0;
-
-  if (soma != cpf[10]) return false;
-
+  count = 0;
+  for (i = 0; i < 9; i++) count += parseInt(cpf.charAt(i)) * (10 - i);
+  dig = 11 - (count % 11);
+  if (dig == 10 || dig == 11) dig = 0;
+  if (dig != parseInt(cpf.charAt(9))) return false;
+  count = 0;
+  for (i = 0; i < 10; i++) count += parseInt(cpf.charAt(i)) * (11 - i);
+  dig = 11 - (count % 11);
+  if (dig == 10 || dig == 11) dig = 0;
+  if (dig != parseInt(cpf.charAt(10))) return false;
+  validateCPF();
   return true;
 }
 
