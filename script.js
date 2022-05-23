7b947a3af992179766932602316aefce0411642f
Gabriel Penteado <105011647+penteado-git@users.noreply.github.com>
2022-05-23 10:40:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,44 @@ const html = {
   btns: [...doc(".btns").children],
 };
 
+function isCPF(numCpf = 0) {
+  numCpf = numCpf.replace(/\.|-/g, "");
+  let soma = 0;
+  soma += cpf[0] * 10;
+  soma += cpf[1] * 9;
+  soma += cpf[2] * 8;
+  soma += cpf[3] * 7;
+  soma += cpf[4] * 6;
+  soma += cpf[5] * 5;
+  soma += cpf[6] * 4;
+  soma += cpf[7] * 3;
+  soma += cpf[8] * 2;
+  soma = (soma * 10) % 11;
+  if (soma == 10 || soma == 11) soma = 0;
+
+  console.log("Primeiro digito : " + soma);
+  if (soma != cpf[9]) return false;
+
+  soma = 0;
+  soma += cpf[0] * 11;
+  soma += cpf[1] * 10;
+  soma += cpf[2] * 9;
+  soma += cpf[3] * 8;
+  soma += cpf[4] * 7;
+  soma += cpf[5] * 6;
+  soma += cpf[6] * 5;
+  soma += cpf[7] * 4;
+  soma += cpf[8] * 3;
+  soma += cpf[9] * 2;
+  soma = (soma * 10) % 11;
+  if (soma == 10 || soma == 11) soma = 0;
+
+  if (soma != cpf[10]) return false;
+
+  console.log("Segundo digito : " + soma);
+  return true;
+}
+
 function validateCPF() {
   const cpf = doc("#cpf").value;
   let regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
