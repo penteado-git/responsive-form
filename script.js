c3f745b2688651c1721d46f6105ed38df0c8526f
Gabriel Penteado <105011647+penteado-git@users.noreply.github.com>
2023-09-28 21:00:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,68 +10,71 @@ const html = {
   separt: doc(".separt"),
 };
 
-function isCPF() {
-  var cpf = doc("#cpf").value;
+function isCPF(cpf) {
   cpf = cpf.replace(/[^\d]+/g, "");
-  if (cpf == "") {
-    alert("Digite o CPF.");
+
+  if (cpf === "") {
+    console.error("Digite o CPF.");
+    return false;
   }
 
-  if (
-    cpf.length > 11 ||
-    cpf == "00000000000" ||
-    cpf == "11111111111" ||
-    cpf == "22222222222" ||
-    cpf == "33333333333" ||
-    cpf == "44444444444" ||
-    cpf == "55555555555" ||
-    cpf == "66666666666" ||
-    cpf == "77777777777" ||
-    cpf == "88888888888" ||
-    cpf == "99999999999"
-  )
+  if (/^(\d)\1{10}$/.test(cpf)) {
+    console.error("CPF Inválido");
     return false;
+  }
+
+  let count = 0;
+  for (let i = 0; i < 9; i++) {
+    count += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let dig = 11 - (count % 11);
+  if (dig === 10 || dig === 11) dig = 0;
+  if (dig !== parseInt(cpf.charAt(9))) {
+    console.error("CPF Inválido");
+    return false;
+  }
 
   count = 0;
-  for (i = 0; i < 9; i++) count += parseInt(cpf.charAt(i)) * (10 - i);
+  for (let i = 0; i < 10; i++) {
+    count += parseInt(cpf.charAt(i)) * (11 - i);
+  }
   dig = 11 - (count % 11);
-  if (dig == 10 || dig == 11) dig = 0;
-  if (dig != parseInt(cpf.charAt(9))) alert("CPF Inválido");
-  count = 0;
-  for (i = 0; i < 10; i++) count += parseInt(cpf.charAt(i)) * (11 - i);
-  dig = 11 - (count % 11);
-  if (dig == 10 || dig == 11) dig = 0;
-  if (dig != parseInt(cpf.charAt(10))) return false;
+  if (dig === 10 || dig === 11) dig = 0;
+  if (dig !== parseInt(cpf.charAt(10))) {
+    console.error("CPF Inválido");
+    return false;
+  }
+
   validateCPF();
-  alert("CPF Válido");
+  console.log("CPF Válido");
   return true;
 }
 
 function validateCPF() {
   const cpf = doc("#cpf").value;
-  let regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
-  try {
-    if (!regex.test(cpf) || cpf === "") throw new Error();
-  } catch (Error) {
+  const regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+
+  if (!regex.test(cpf) || cpf === "") {
     return;
   }
+
   validateDate();
   validaName();
 }
 
 function validaName() {
-  nome = doc("#nome").value;
+  const nome = doc("#nome").value;
   if (nome === "") {
-    alert("Insira seu nome");
-    nome.focus();
+    console.error("Insira seu nome");
+    doc("#nome").focus();
   }
   localStorage.setItem("nome", nome);
 }
 
 function validateDate() {
   const dtNasc = doc("#dtNasc").value;
-  if (dtNasc == "") {
-    alert("Insira a data de nascimento");
+  if (dtNasc === "") {
+    console.error("Insira a data de nascimento");
     return;
   }
   showCurrentStep("adress");
@@ -86,17 +89,16 @@ function hideAllStepContent() {
 
 function removeAllActiveStep() {
   html.links.forEach((step) => {
-    step.className = step.className.replace("active", "");
+    step.classList.remove("active");
   });
 }
 
 function showCurrentStep(id) {
   hideAllStepContent();
-
   const stepContent = doc("#" + id);
   stepContent.style.display = "block";
+  stepContent.classList.add("active");
 
-  id.className += "active";
   if (id === "final") {
     show();
   }
@@ -104,11 +106,9 @@ function showCurrentStep(id) {
 
 function selectStep(event) {
   removeAllActiveStep();
-
   const select = event.currentTarget;
   showCurrentStep(select.dataset.id);
-
-  select.className += "active";
+  select.classList.add("active");
 }
 
 function listenForChanges() {
@@ -143,14 +143,16 @@ function mascaraCpf(mascara, input) {
 function consultCEP() {
   const cep = doc("#cep").value;
 
-  if (cep.length != 8 || cep === "") {
-    alert("CEP inválido");
+  if (cep.length !== 8 || cep === "") {
+    console.error("CEP inválido");
     return;
-  } else var url = "https://viacep.com.br/ws/" + cep + "/json/";
+  }
+
+  const url = "https://viacep.com.br/ws/" + cep + "/json/";
   $.getJSON(url, function (data) {
-    $("#rua").val(data.logradouro);
-    $("#bairro").val(data.bairro);
-    $("#number").val(data.number);
+    doc("#rua").value = data.logradouro;
+    doc("#bairro").value = data.bairro;
+    doc("#number").value = data.number;
     localStorage.setItem("rua", data.logradouro);
     localStorage.setItem("bairro", data.bairro);
     localStorage.setItem("cidade", data.localidade);
@@ -158,33 +160,30 @@ function consultCEP() {
 }
 
 function showFinal() {
-  let name = doc("#nome").value;
-  let dtNasc = doc("#dtNasc").value;
-  let cpf = doc("#cpf").value;
-  html.final.innerHTML = `<p>Nome: ${name}<\p>
-                          <p>Data de Nascimento: ${dtNasc}<\P>
-                          <p>CPF: ${cpf}<\P>`;
+  const name = doc("#nome").value;
+  const dtNasc = doc("#dtNasc").value;
+  const cpf = doc("#cpf").value;
+  html.final.innerHTML = `<p>Nome: ${name}</p>
+                          <p>Data de Nascimento: ${dtNasc}</p>
+                          <p>CPF: ${cpf}</p>`;
 }
+
 function show() {
   html.separt.innerHTML = `<p>${localStorage.getItem(
     "rua",
     data.logradouro
-  )}<\p>
+  )}</p>
                           <p>Bairro: ${localStorage.getItem(
                             "bairro",
                             data.bairro
-                          )}<\p>
+                          )}</p>
                           <p>Cidade: ${localStorage.getItem(
                             "cidade",
                             data.localidade
-                          )}<\p>`;
+                          )}</p>`;
 }
 
 function start() {
   hideAllStepContent();
   listenForChanges();
   localStorage.clear();
-  html.openStep.click();
-}
-
-start();
